fc26bd3321ffceee5909b8674ce9794643b0c282
saurabh Joshi <95601849+Saurabh9702@users.noreply.github.com>
2023-09-20 15:30:53 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,22 @@
+
+package universitymangementsystem;
+
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "saurabh");
+            s = c.createStatement();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
